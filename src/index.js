7251129617bce42dce0e9d8d6580db279ed1f0a5
shna rafeeq <shnar.majeed@gmail.com>
2020-05-19 10:15:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let listUl = document.getElementById("tasks");
+
+  document.querySelector("#btn-submit").addEventListener("click", function(event) {
+         event.preventDefault();
+         let inputText = document.getElementById("new-task-description").value;
+         let li = document.createElement("li");
+         li.innerText = inputText;
+         listUl.appendChild(li);
+}, false);
+
+
+
+
 });
